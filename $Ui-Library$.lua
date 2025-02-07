4ddbad2f7985e3cda7b6bbb1161796c2681aa88f
DarkOSVO <167088508+DarkOSVO@users.noreply.github.com>
2025-02-07 13:48:19 +0300

    $Ui-Library$.lua

--- $Ui-Library$.lua
+++ $Ui-Library$.lua
@@ -73,7 +73,7 @@ local themeStyles = {
     BloodTheme = {
         SchemeColor = Color3.fromRGB(0, 102, 0),
         Background = Color3.fromRGB(0, 0, 0),
-        Header = Color3.fromRGB(0, 0, 0),
+        Header = Color3.fromRGB(0, 0, 255),
         TextColor = Color3.fromRGB(0, 255, 0),
         ElementColor = Color3.fromRGB(0, 0, 0)
     },
